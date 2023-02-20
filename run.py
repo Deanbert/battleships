1e1708e8d5035dfe1c1a3736b6ea09d483982c0a
Deanbert <deanfay2000@gmail.com>
2023-02-20 20:47:43 +0000

    add function to print hangman pieces for incorrect guesses and update welcome message

--- run.py
+++ run.py
@@ -3,13 +3,19 @@ import random
 
 #print a welcome for the user and invite them to play
 print("Welcome to Hangman!")
-name = input("What's your name? ")
-print(f"Hello {name}!")
+name = input("What should we call you? ")
+print(f"Hello {name}! Let's play")
+print("You must complete the game within 6 guesses... OR ELSE!!!")
+print("\n+---+")
+print(" O   |")
+print("/|\  |")
+print("/ \  |")
+print("    ===")
 print("-------------------------------------------")
 
 
 #add difficulty selection for users that increases the letter count relative to difficulty selected
-difficulty = input("Please select the difficulty level\n easy - 6 Letter words,\n medium - 7 Letter words,\n hard - 8 Letter words):\n ")
+difficulty = input("Select your preferred difficulty level\n easy - 6 Letter words,\n medium - 7 Letter words,\n hard - 8 Letter words):\n ")
 while difficulty not in ['easy', 'medium', 'hard']:
   print("Invalid input. Please select a valid difficulty level.")
   difficulty = input("Please select the difficulty level (easy, medium, or hard): ")
@@ -22,3 +28,51 @@ elif difficulty == 'medium':
   wordDictionary = ["buzzcut", "qualify", "salvage", "sunbeam", "reading", "witness", "stencil", "costume", "grimace", "serving"]
 else:
   wordDictionary = ["illusion", "teaching", "policies", "exorcist", "stumbled", "invested", "pregnant", "hydrated", "tapestry", "remarked"]
+
+
+def print_hangman(wrong):
+    """
+    Prints a different piece of the hangman character depending on the number of incorrect guesses
+    """
+    if(wrong == 0):
+        print("\n+---+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 1): 
+        print("\n+---+")
+        print("O   |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 2):
+        print("\n+---+")
+        print("O   |")
+        print("|   |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 3):
+        print("\n+---+")
+        print(" O  |")
+        print("/|  |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 4):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 5):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("/   |")
+        print("   ===")
+    elif(wrong == 6):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("/ \  |")
+        print("    ===")

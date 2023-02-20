61b8d2f7a5863b239c1bc6d001c10cbfe3c01a23
Deanbert <deanfay2000@gmail.com>
2023-02-20 19:48:07 +0000

    fix messy comments

--- run.py
+++ run.py
@@ -1,23 +1,21 @@
 import random
 
+
 #print a welcome for the user and invite them to play
 print("Welcome to Hangman!")
 name = input("What's your name? ")
 print(f"Hello {name}!")
 print("-------------------------------------------")
 
-"""
-easy, medium and hard difficulty selection for users that increases 
-letter count of the words to increase difficulty
-"""
+
+#add difficulty selection for users that increases the letter count relative to difficulty selected
 difficulty = input("Please select the difficulty level\n easy - 6 Letter words,\n medium - 7 Letter words,\n hard - 8 Letter words):\n ")
 while difficulty not in ['easy', 'medium', 'hard']:
   print("Invalid input. Please select a valid difficulty level.")
   difficulty = input("Please select the difficulty level (easy, medium, or hard): ")
 
-"""
-chooses a word dictionary based on the users selected difficulty 
-"""
+
+#chooses a word dictionary based on the users selected difficulty 
 if difficulty == 'easy':
   wordDictionary = ["pickax", "whacky", "quacks", "boozey", "joyful", "chubby", "enzyme", "hotdog", "cheese", "jacket"]
 elif difficulty == 'medium':
